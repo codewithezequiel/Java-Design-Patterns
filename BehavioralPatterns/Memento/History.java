243f6f1786c97f9f68acfc8bac13ca18c722418d
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-02-15 21:16:33 -0800

    Memento pattern implementation.

---
+++ BehavioralPatterns/Memento/History.java
@@ -0,0 +1,22 @@
+package BehavioralPatterns.Memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class History {
+    private List<DocumentState> states = new ArrayList<>();
+
+    public void push(DocumentState state) {
+        states.add(state);
+    }
+
+    public DocumentState pop() {
+        var lastIndex = states.size() - 1;
+        var lastState = states.get(lastIndex);
+        states.remove(lastState);
+
+        return lastState;
+
+    }
+
+}
